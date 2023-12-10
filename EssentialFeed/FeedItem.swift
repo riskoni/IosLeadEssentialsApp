3a1bdfe5a41c8e087b5b02792bf55c75111f9863
Nikolay Riskov <riskoni>
2023-12-10 15:35:50 +0200

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Nikolay Riskov on 10.12.23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
