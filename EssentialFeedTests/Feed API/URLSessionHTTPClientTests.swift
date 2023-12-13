6a7513a02829107c7e7486b060155fbbecbf13b0
Nikolay Riskov <riskoni>
2023-12-13 15:31:26 +0200

    Add `makeSUT` factory method to protect tests from breaking changes

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -48,8 +48,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
        
-        let sut = URLSessionHTTPClient()
-        sut.get(from: url){ _ in }
+        makeSUT().get(from: url){ _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -58,11 +57,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         let url = URL(string: "https://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
-       
-        let sut = URLSessionHTTPClient()
-        
+               
         let exp = expectation(description: "wait for completion")
-        sut.get(from: url){ result in
+        makeSUT().get(from: url){ result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(error.code, receivedError.code)
@@ -78,6 +75,10 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
 
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
