9dfc4cf79fd6ebf215a5ad4eb5b2e80e28518083
Nikolay Riskov <riskoni>
2023-12-16 22:57:20 +0200

    Save command requests cache deletion

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUsetestCaseTests.swift
@@ -0,0 +1,60 @@
+//
+//  CacheFeedUsetestCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Nikolay Riskov on 16.12.23.
+//
+
+import XCTest
+import EssentialFeed
+
+class LocalFeedLoader {
+    private let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
+    }
+}
+
+class FeedStore {
+    var deleteCacheFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCacheFeedCallCount += 1
+    }
+}
+
+final class CacheFeedUsetestCaseTests: XCTestCase {
+
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+    
+    
+    // MARK: - Helpers
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any description", location: "any location", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
+    }
+    
+}
